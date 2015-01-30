b2dcb1fbdb242c2efca3133a233a57dce6e12097
Aaron Chambers <achambers@gmail.com>
2015-01-30 09:29:57 +0000

    Now we have a {{content-for 'caliper'}} tag
    This tag will inject the caliper config into the HEAD of he document.
    
    The addon will also include the caliper.js file once it has been
    installed with bower

--- index.js
+++ index.js
@@ -2,5 +2,21 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-caliper'
+  name: 'ember-caliper',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/caliper-ember/dist/caliper.ember.min.js');
+  },
+
+  contentFor: function(type, config) {
+    if (type === 'caliper') {
+      var caliper = config.caliper || {};
+      var options = {config: caliper}
+      var result  = '<script type="text/javascript">var Caliper = ' + JSON.stringify(options) + ';</script>';
+
+      return result;
+    }
+  }
 };
